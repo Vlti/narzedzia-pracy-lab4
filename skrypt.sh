abafa4d4b0b9635f812a694170495a6e2da12ff1
Vlti <jan.fasciszewski@protonmail.com>
2023-05-06 15:27:05 +0200

    6

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" ]] || [[ "$1" == "-d" ]]; then
 	echo "$(date + %d-%m-%Y)"
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "--logs" ]] || [[ "$1" == "-l" ]]; then
 	for i in $(seq 1 "$2"); do
 		filename="log${i}.txt"
 		echo "$filename" >> $filename
 		echo "$0" >> $filename
 		echo "$(date)" >> $filename
 	done
-elif [[ "$1" == "--help" ]]; then
+elif [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
 	echo "usage: skrypt.sh [OPTION] [NUMBER]"
 	echo "--date		Prints out current date"
 	echo "--logs [N]	Creates N files named logx.txt, where x is a number from 1 to N"
