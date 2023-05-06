172046006ddfdbe2b2b2dd33a69b3ec4b0ce5035
Vlti <jan.fasciszewski@protonmail.com>
2023-05-06 15:12:53 +0200

    4

--- skrypt.sh
+++ skrypt.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "--date" ]]; then
 	echo "$(date + %d-%m-%Y)"
 elif [[ "$1" == "--logs" ]]; then
-	for i in $(seq 1 100); do
+	for i in $(seq 1 "$2"); do
 		filename="log${i}.txt"
 		echo "$filename" >> $filename
 		echo "$0" >> $filename
