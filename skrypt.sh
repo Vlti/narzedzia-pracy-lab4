d6b98b8aa0c8644dd8c7f93d0fc7a4891e4218d9
Vlti <jan.fasciszewski@protonmail.com>
2023-05-06 15:09:31 +0200

    3

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,13 @@
 
 if [[ "$1" == "--date" ]]; then
 	echo "$(date + %d-%m-%Y)"
+elif [[ "$1" == "--logs" ]]; then
+	for i in $(seq 1 100); do
+		filename="log${i}.txt"
+		echo "$filename" >> $filename
+		echo "$0" >> $filename
+		echo "$(date)" >> $filename
+	done
 else
 	echo "Error!"
 fi
